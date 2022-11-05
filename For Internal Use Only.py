07f8115b3cdef82ee53670b13e044c6f11e17b6f
Octavius1692 <117535880+Octavius1692@users.noreply.github.com>
2022-11-05 14:50:27 -0400

    Add files via upload

---
+++ For Internal Use Only.py
@@ -0,0 +1,9 @@
+import praw
+reddit = praw.Reddit(
+    client_id='Cj9-71XrLwKx-f6yh1xBXQ',
+    client_secret='31l_JzqFAjGzl_IfVsCuOktlUImZOA',
+    user_agent="windows:Crawler2:V1(by u/watercress97)"
+)
+print(reddit.read_only)
+for submission in reddit.subreddit("gonewild").hot(limit=10):
+    print(submission.title)
