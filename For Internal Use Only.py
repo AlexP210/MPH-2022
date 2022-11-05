d8c04ef8ce0c94baf64aa2f6914fed58eceecfd1
Octavius1692 <117535880+Octavius1692@users.noreply.github.com>
2022-11-05 15:54:15 -0400

    Add files via upload

--- For Internal Use Only.py
+++ For Internal Use Only.py
@@ -1,9 +1,24 @@
+from wsgiref.util import request_uri
 import praw
+import wget
 reddit = praw.Reddit(
     client_id='Cj9-71XrLwKx-f6yh1xBXQ',
     client_secret='31l_JzqFAjGzl_IfVsCuOktlUImZOA',
     user_agent="windows:Crawler2:V1(by u/watercress97)"
 )
 print(reddit.read_only)
-for submission in reddit.subreddit("itookapicture").hot(limit=10):
-    print(submission.title)
+i = 0
+for submission in reddit.subreddit("noncredibledefense").top(limit=10):
+    i = i + 1
+    # img_data = requests.get(submission.url).content
+    print(submission.url)
+# response = wget.download(submission.url, "yoda.gif")
+    url = submission.url
+    urls = submission.url
+    a = urls.split('.') [-1]
+    if a in 'jpg' 'png':
+        print(a)
+        response = wget.download(url, str(i)+ '.' + str(a))
+    else:
+        print("invalid filetype")
+print(i)
